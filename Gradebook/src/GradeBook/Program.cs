8d7c5975c9e7befc6acaef8d11d7b9b94cd85e90
Matias.Galvez <49412396+matiasgalvez@users.noreply.github.com>
2019-10-25 13:54:19 -0300

    Add files via upload

--- Gradebook/src/GradeBook/Program.cs
+++ Gradebook/src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,11 +7,42 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            var i = 0;
+            var sum = 0.0;
+            //var numbers = new[] {1.5, 2.5, 3.5};
+            var grades = new List<double>() {1.522, 2.544, 3.555};
+            grades.Add(4.666);
+            foreach (var number in grades) {
+                sum += number;
+            }
+            i = grades.Count;
+            //System.Console.WriteLine(i);
+            var avg = sum/i;
+            //System.Console.WriteLine(sum);
+            System.Console.WriteLine($"The average Grade is {avg:N1}");
+            //var sum2 = 0.0;
+
+            /*for (var i = 0; i<numbers.Length; i++){
+                sum = sum + numbers[i];
+                System.Console.WriteLine(i);
+            }*/
+            /*foreach (var number in numbers) {
+                sum2 += number;
+            }
+            Console.WriteLine(sum2);*/
+
             if (args.Length > 0) {
                 Console.WriteLine($"Hello, {args[0]}!");
             }else {
                 Console.WriteLine("Hello");
             }
+
+            //var result = Average(x, y);
+            //Console.WriteLine(result);
+        }
+        static double Average(double x, double y)
+        {
+            return ((x+y)/2);
         }
     }
 }
